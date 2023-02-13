b289105753473f35dc27bde80395b576d163f3be
ohshitnotgood <praanto@icloud.com>
2023-02-13 14:56:22 +0100

    Somewhat working y-coordinate

--- ios-client/ios-client/ContentView.swift
+++ ios-client/ios-client/ContentView.swift
@@ -9,51 +9,164 @@ import SwiftUI
 
 /// Main entry point view of the app.
 struct ContentView: View {
-    @State private var showCoordinatesHistory   = false
-    @State private var showAppLog               = false
-    @State private var showCreditsSheet         = false
+    @State private var showCoordinatesHistory       = false
+    @State private var showAppLog                   = false
+    @State private var showCreditsSheet             = false
+    @State private var showConnectToBoardAlert      = false
+    @State private var showRefreshRateAlert         = false
+    @State private var showFeatureNotImplemented    = false
     
-    @StateObject private var kit              = MotionKit()
+    @State private var boardIPAddress               = ""
+    
+    @ObservedObject private var kit                 = MotionKit()
+    @ObservedObject private var settings            = SettingsValueStore()
     
     var body: some View {
         NavigationView {
-            VStack {
-                LogItemView(
-                    messageTitle: "Next coordinates",
-                    messageBody: "(x: \(kit.latestCoordinate.x), y: \(kit.latestCoordinate.y))")
-                
-                
-                LogItemView(
-                    messageTitle: "Current refresh rate",
-                    messageBody: "100Hz")
-                
-                LogItemView(
-                    messageTitle: "Connection status",
-                    messageBody: "Disconnected")
-            }
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Menu {
-                        Button {
-                            showCoordinatesHistory.toggle()
-                        } label: {
-                            Label("View coordinates history", systemImage: "graph")
-                        }
-                        
-                        Button("Adjust refresh rate") {
-                            
-                        }
-                        
-                        Button("Connect to board") {
-                            
-                        }
-                        
-                        Button("Credits") {
-                            showCreditsSheet.toggle()
-                        }
-                    } label: {
-                        Label("Menu", systemImage: "ellipsis.circle")
+            List {
+                Section {
+                    HStack {
+                        Text("Xpc, Ypc")
+                        Spacer()
+                        Text("\(kit.x), \(kit.y)")
+                            .foregroundColor(.secondary)
+                            .monospaced()
                     }
+                } header: {
+                    Text("Coordinates")
+                }
+                
+                Section {
+                    HStack {
+                        Text("Sensor refresh rate")
+                        Spacer()
+                        Text("\(settings.sensorDataRefreshRate)Hz")
+                            .foregroundColor(.secondary)
+                            .monospaced()
+                    }
+                    
+                    
+                    HStack {
+                        Text("Sensitivity")
+                        Spacer()
+                        Text("\(settings.sensitivity)")
+                            .foregroundColor(.secondary)
+                            .monospaced()
+                    }
+                    
+                    HStack {
+                        Text("Connection Status")
+                        Spacer()
+                        Text("Disconnected")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("IP Address")
+                        Spacer()
+                        Text("-")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("Latency")
+                        Spacer()
+                        Text("-")
+                            .foregroundColor(.secondary)
+                    }
+                } header: {
+                    Text("Configurations")
+                }
+                
+                #if DEBUG
+//                Section {
+//                    HStack {
+//                        Text("Pitch (x)")
+//                        Spacer()
+//                        Text("\(kit.latestCoordinate.pitch)")
+//                            .foregroundColor(.secondary)
+//                    }
+//
+//                    HStack {
+//                        Text("Roll (y)")
+//                        Spacer()
+//                        Text("\(kit.latestCoordinate.roll)")
+//                            .foregroundColor(.secondary)
+//                    }
+//
+//                    HStack {
+//                        Text("Yaw (z)")
+//                        Spacer()
+//                        Text("\(kit.latestCoordinate.yaw)")
+//                            .foregroundColor(.secondary)
+//                    }
+//                } header: {
+//                    Text("Attitude Data")
+//                }
+                
+                Section {
+                    HStack {
+                        Text("x")
+                        Spacer()
+                        Text("\(kit.accl_x)")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("y")
+                        Spacer()
+                        Text("\(kit.accl_y)")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("z")
+                        Spacer()
+                        Text("\(kit.accl_z)")
+                            .foregroundColor(.secondary)
+                    }
+                } header: {
+                    Text("Accelerometer Data")
+                }
+                
+                Section {
+                    HStack {
+                        Text("x")
+                        Spacer()
+                        Text("\(kit.tilt_x)")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("y")
+                        Spacer()
+                        Text("\(kit.tilt_y)")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("z")
+                        Spacer()
+                        Text("\(kit.tilt_z)")
+                            .foregroundColor(.secondary)
+                    }
+                } header: {
+                    Text("Gyro Data")
+                }
+                
+                #endif
+                
+                Section {
+                    Button("Connect to the board") {
+//                        showConnectToBoardAlert.toggle()
+                        showFeatureNotImplemented.toggle()
+                    }
+                    Button("View network log") {
+//                        showFeatureNotImplemented.toggle()
+                        kit.latestCoordinate.x = 89
+                    }
+                } header: {
+                    Text("Network")
                 }
             }
             .sheet(isPresented: $showCoordinatesHistory, content: {
@@ -62,8 +175,44 @@ struct ContentView: View {
             .sheet(isPresented: $showCreditsSheet, content: {
                 CreditsView()
             })
+            .alert("Adjust refresh rate", isPresented: $showRefreshRateAlert, actions: {
+                Slider(
+                    value: $settings.sensorDataRefreshRate,
+                    in: 1...100
+                )
+            }, message: {
+                Text("Slide to adjust the sensor refresh rate")
+            })
+            .alert("Connect to the chip kit", isPresented: $showConnectToBoardAlert, actions: {
+                TextField("URL", text: $boardIPAddress)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                Button("Cancel", role: .destructive, action: {
+                    
+                })
+                
+                Button("Connect", role: .cancel, action: {
+                    
+                })
+            }, message: {
+                Text("Enter the IP address to establish connection with the board.")
+            })
+            .alert("Feature not implemented", isPresented: $showFeatureNotImplemented, actions: {
+                Button("Cancel", role: .cancel) {
+                    
+                }
+            }, message: {
+                Text("This feature has not been implemented and thus cannot be invoked.")
+            })
+            .onAppear {
+                do {
+                    try kit.startUpdatingCoordinates()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
             .navigationTitle("ios-client")
-            .padding()
         }
     }
 }
@@ -72,20 +221,20 @@ struct LogItemView: View {
     let messageTitle: String
     let messageBody: String
     var body: some View {
-        VStack {
+        HStack {
             Text(messageTitle)
+            Spacer()
             Text(messageBody)
-                .monospaced()
         }.padding()
     }
 }
 
 struct CreditsView: View {
     var body: some View {
-        ScrollView {
-            Text("Acknowledgements")
-                .bold()
-                .font(.title)
+        NavigationView {
+            VStack {
+                
+            }.navigationTitle("Acknowledgements")
         }
     }
 }
@@ -100,9 +249,18 @@ struct CoordinatesListingView: View {
                     Text("20.321\n21.3212")
                         .monospaced()
                 }
-            }.navigationTitle("Coordinates History")
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Coordinates History")
         }
     }
+}
+
+/// Stores settings data for the app.
+class SettingsValueStore: ObservableObject {
+    @Published var sensorDataRefreshRate = 100.0
+    @Published var connectionStatus = "Disconnected"
+    @Published var sensitivity = 6
 }
 
 @available(*, unavailable)
--- ios-client/ios-client/MotionKit.swift
+++ ios-client/ios-client/MotionKit.swift
@@ -14,19 +14,122 @@ import CoreMotion
 /// `MotionKit` translates accelerometer data from an iOS device into 
 /// game commands for the u32 system.
 class MotionKit: ObservableObject {
-
+    
     /// Contains `x` and `y` coordinate of the next position of the paddle on the OLED display.
     @Published var latestCoordinate = MKCoordinate()
     
-    private var manager = CMMotionManager()
+    // MARK: Published variables
+    @Published var accl_x   = 0.0
+    @Published var accl_y   = 0.0
+    @Published var accl_z   = 0.0
     
-    func startUpdatingCoordinates() {
-        
+    @Published var tilt_x   = 0
+    @Published var tilt_y   = 0
+    @Published var tilt_z   = 0
+    
+    @Published var x = 0 {
+        didSet {
+            if self.x > 120 { self.x = 120 }
+            if self.x > 0 { self.x = 0 }
+        }
+    }
+    @Published var y = 0 {
+        didSet {
+            if self.y > 30 { self.y = 30 }
+            if self.y < 0 { self.y = 0 }
+        }
     }
     
+    @Published var speed    = 0
+    
+    
+    private var queue       = OperationQueue()
+    private var manager     = CMMotionManager()
+    
+    /// Starts updating values in ``latestCoordinate``.
+    func startUpdatingCoordinates() throws {
+        if !manager.isAccelerometerAvailable {
+            throw AccelerometerNotAvailableError()
+        }
+        
+        if !manager.isGyroAvailable {
+            throw GyroNotAvailableError()
+        }
+        
+        self.manager.deviceMotionUpdateInterval = 1.0 / 60.0
+        self.manager.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: self.queue) { [self] data, error in
+            if let validData = data {
+                computeAcceleration(validData.userAcceleration)
+                computeXYCoordinate(validData.userAcceleration)
+            }
+        }
+        
+        self.manager.startGyroUpdates(to: self.queue) { [self] data, error in
+            if let data = data {
+                computeTilt(data)
+            }
+        }
+    }
+    
+    /// Ends updating values in ``latestCoordinate``.
     func endUpdatingCoordinates() {
-        
+        self.manager.stopAccelerometerUpdates()
+        self.manager.stopDeviceMotionUpdates()
+        self.manager.stopGyroUpdates()
     }
+    
+    // MARK: Sensor Data Comp Funcs
+    private func computeAcceleration(_ accelerationData: CMAcceleration) {
+        DispatchQueue.main.async {
+            self.accl_x = accelerationData.x
+            self.accl_y = accelerationData.y
+            self.accl_z = accelerationData.z
+        }
+    }
+    
+    private func computeTilt(_ tiltData: CMGyroData) {
+        DispatchQueue.main.async {
+            self.tilt_x = Int(tiltData.rotationRate.x)
+            self.tilt_y = Int(tiltData.rotationRate.y)
+            self.tilt_z = Int(tiltData.rotationRate.z)
+        }
+    }
+    
+    // MARK: computeXYCoordinate
+    private func computeXYCoordinate(_ accelerationData: CMAcceleration) {
+        DispatchQueue.main.async { [self] in
+            // Assuming the phone is being held in portrait, screen facing user.
+            let acc_data_x = accelerationData.x * 500
+            let acc_data_y = accelerationData.y * 500
+            let dist_x = getCalibratedCoordinate(acc_data_x)
+            let dist_y = getCalibratedCoordinate(acc_data_y)
+            
+            x += Int(dist_x)
+            y += Int(dist_y)
+        }
+    }
+    
+    // MARK: getCalibratedCoordinates
+    private func getCalibratedCoordinate(_ accData: Double) -> Double {
+        var calibratedCoordinate: Double = 0.0
+        
+        if accData > (2 * 500) {
+            calibratedCoordinate = 120.0
+        } else if accData < (-2 * 500) {
+            calibratedCoordinate = 0.0
+        }
+        
+        if abs(accData) > 10 {
+            calibratedCoordinate = 1.0 * accData.signOf()
+        } else if abs(accData) > 20 {
+            calibratedCoordinate = 2.0 * accData.signOf()
+        } else if abs(accData) > 30 {
+            calibratedCoordinate = 3.0 * accData.signOf()
+        }
+        
+        return calibratedCoordinate
+    }
+    
 }
 
 /// Encapsulates the x and y coordinate of the user's racquet while also ensuring that the coordinates do not go out of display bounds.
@@ -45,4 +148,21 @@ class MKCoordinate {
     
 }
 
+class AccelerometerNotAvailableError: Error {
+    
+}
 
+
+class GyroNotAvailableError: Error {
+    
+}
+
+extension Double {
+    func signOf() -> Int {
+        return self > 0 ? 1 : -1
+    }
+    
+    func signOf() -> Double {
+        return self > 0 ? 1.0 : -1.0
+    }
+}
