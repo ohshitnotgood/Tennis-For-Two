71138da094785af6c5dd9d6087cda1364aca1bf3
ohshitnotgood <praanto@icloud.com>
2023-02-14 17:33:57 +0100

    Initialised Android project

---
+++ android-client/app/src/main/java/se/kth/is1200/android_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package se.kth.is1200.android_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
