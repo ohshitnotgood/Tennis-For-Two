063481e7b8b65ef43f19d2b13dc576024e850a37
ohshitnotgood <praanto@icloud.com>
2023-02-16 21:45:04 +0100

    Create NetworkKit

---
+++ ios-client/ios-client/NetworkKit.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkKit.swift
+//  ios-client
+//
+//  Created by Praanto on 2023-02-16.
+//
+
+import Foundation
+
+class NetworkKit: ObservableObject {
+    func connectToBoard() {
+        
+    }
+}
